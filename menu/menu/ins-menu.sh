3764bc97528d96ebc927f094fb48d177c2590490
richnstore <zianfth@gmail.com>
2024-07-22 04:51:28 +0700

    Delete menu directory

--- menu/menu/ins-menu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-clear
-wget "https://raw.githubusercontent.com/richnstore/scriptbyrich/main/menu.zip"
-    unzip menu.zip
-    chmod +x menu/*
-    mv menu/* /usr/bin
-    rm -rf menu
-    rm -rf menu.zip
