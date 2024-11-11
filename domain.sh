3beda103217cca9500da670c8b07f695fa464761
richnstore <zianfth@gmail.com>
2024-11-11 12:54:54 +0700

    Add files via upload

---
+++ domain.sh
@@ -0,0 +1,15 @@
+yellow "Add Domain"
+echo " "
+read -rp "Input domain : " -e pp
+    if [ -z $pp ]; then
+        echo -e "
+        Nothing input for domain!
+        Then a random domain will be created"
+    else
+        echo "$pp" > /root/scdomain
+	echo "$pp" > /etc/xray/scdomain
+	echo "$pp" > /etc/xray/domain
+	echo "$pp" > /etc/v2ray/domain
+	echo $pp > /root/domain
+        echo "IP=$pp" > /var/lib/richnstore/ipvps.conf
+    fi
